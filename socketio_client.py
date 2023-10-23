b67cefe3151048d6bc1e0395f13fb859e0a37a65
Himel <shariar076>
2023-10-23 13:18:36 +0600

    update

--- socketio_client.py
+++ socketio_client.py
@@ -18,38 +18,51 @@ async def session_confirm(room_id):
 
 
 @sio_client.event
-async def bot_uttered(data):
-    print('reply: ', data['text'])
-    # next message
-    message = input("message: ")
-    data = {"session_id": sio_client.sid, "message": message}
-    await sio_client.emit('user_uttered', data)
-
-
-@sio_client.event
 async def disconnect():
     print('I\'m disconnected')
 
 
+@sio_client.event
+async def bot_uttered(data):
+    print('reply: ', data['text'])
+    # next message
+    message = input("message: ")
+    if message=='exit':
+        await sio_client.disconnect()
+    else:
+        data = {"session_id": sio_client.sid, "message": message}
+        await sio_client.emit('user_uttered', data)    
+    
+
+
 async def main():
     # await sio_client.connect(url='http://0.0.0.0:8000', socketio_path='/chatbot/socket.io')
-    await sio_client.connect(url='http://0.0.0.0:5005', socketio_path='socket.io')
+    await sio_client.connect(url='http://0.0.0.0:5005', socketio_path='socket.io', transports=['websocket'])
 
-    # await sio_client.send('chat', {'message': "message"})
-    # await sio_client.emit('chat', data)
-
-    # event = await bot_uttered(data)
-    # print(f'received event: "{event[0]}" with arguments {event[1:]}')
     message = "Hello" # 1st message
     data = {"session_id": sio_client.sid, "message": message}
     await sio_client.emit('user_uttered', data)
+
     # event = await sio_client.on("bot_uttered")
     # print(event)
-    await sio_client.wait()
-    # event = await sio_client.event("bot_uttered")
     # print(f'received event: "{event[0]}" with arguments {event[1:]}')
 
-    await sio_client.disconnect()
+    await sio_client.wait()
+
 
 
 asyncio.run(main())
+
+
+"""
+Exception occurred while handling uri: 'ws://0.0.0.0:5005/socket.io/?transport=websocket&EIO=4&t=1698045340.2737615'
+Traceback (most recent call last):
+  File "/home/himel/Documents/ChatBot/venv_rasa_305/lib/python3.8/site-packages/sanic/app.py", line 782, in handle_request
+    raise ServerError(
+sanic.exceptions.ServerError: Invalid response type None (need HTTPResponse)
+
+this is a known issue:
+https://github.com/miguelgrinberg/python-engineio/issues/221
+https://github.com/sanic-org/sanic/issues/2105
+https://github.com/sanic-org/sanic/issues/2572
+"""
